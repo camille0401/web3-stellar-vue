1cbf9603be6cdafe0bf9c556d89a3b762bc48d19
wangdery <wangdery819@gmail.com>
2025-04-11 12:03:26 +0800

    deposit-update

--- src/main.js
+++ src/main.js
@@ -5,9 +5,13 @@ import i18n from "./i18n";
 
 import "aos/dist/aos.css";
 import "./css/style.css";
+import ToastPlugin from 'vue-toast-notification';
+import 'vue-toast-notification/dist/theme-sugar.css';
 
 const app = createApp(App);
 app.use(router);
 app.use(i18n);
-
+app.use(ToastPlugin, {
+    position: 'top-right'
+  });
 app.mount("#app");
